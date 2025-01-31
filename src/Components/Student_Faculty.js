d97574af2039b184e14ccc205d67c3610ff60e51
ItsUr-Manikanta-18 <manikantaalamanda5@gmail.com>
2025-01-31 22:54:39 +0530

    first commit

--- src/Components/Student_Faculty.js
+++ src/Components/Student_Faculty.js
@@ -7,7 +7,7 @@ const Student_Faculty=(props)=>{
         <>
             <Header name={props.headername} />
             <div className='student_faculty_total'>
-                <Link to='student/branches' className="link"><div>Student<div className='student'></div></div></Link>
+                <Link to='student/branches' className="link"><div>Studentsss<div className='student'></div></div></Link>
                 <Link to='faculty/branches' className="link"><div>Faculty<div className='faculty'></div></div></Link>
             </div>
             <Footer previouspath={props.previouspath}/>
